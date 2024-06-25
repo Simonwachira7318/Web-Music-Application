7453ec2c98664cb4c2e5d3921e17595d1f358270
SIMON WACHIRA <simonwachira7318@gmail.com>
2024-06-25 14:07:00 +0300

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,38 @@
+let songs = [
+  {
+    name: "On Me",
+    artist: "Lil Baby",
+    img: "lil-baby",
+    coverImg: "lil-baby",
+    audio: "onme"
+  },
+  {
+    name: "Run it Up",
+    artist: "Lil Tjay ft Offset & Moneybagg",
+    img: "lil-tjay",
+    coverImg: "lil-tjay",
+    audio: "runitup"
+  },
+  {
+    name: "Pressa Attachments",
+    artist: "Coi Leray",
+    img: "coi-leray",
+    coverImg: "coi-leray",
+    audio: "pressaattachments"
+  },
+  {
+    name: "IDGAF",
+    artist: "Toosii",
+    img: "toosii1",
+    coverImg: "toosii1",
+    audio: "toosiiIDGAF"
+  }
+  ,
+  {
+    name: "Love is ...",
+    artist: "Toosii",
+    img: "toosii22",
+    coverImg: "itoosii22",
+    audio: "loveis"
+  }
+]
